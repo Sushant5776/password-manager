334d864710404b617a0f1907af3be19c533ca288
Sushant Garudkar <garudkarsushant7@gmail.com>
2022-06-10 16:36:16 +0530

    Removal of Exposed Database Env Variables

--- utils/database/index.ts
+++ utils/database/index.ts
@@ -15,12 +15,12 @@ import {
 } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCPzwN8xh-g8opC-itI2dPRWECt4fTfLlc',
-  authDomain: 'passman-26534.firebaseapp.com',
-  projectId: 'passman-26534',
-  storageBucket: 'passman-26534.appspot.com',
-  messagingSenderId: '611423179753',
-  appId: '1:611423179753:web:bf2ef0e540bcb76a783178',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
