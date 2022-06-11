3f54f2c26634a35896d3417aabe37326b632e785
Sushant Garudkar <garudkarsushant7@gmail.com>
2022-06-11 23:33:07 +0530

    Dark Mode UI (Authenticated Part) Dashboard Update

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -93,13 +93,13 @@ const Dashboard: NextPage<{ session: Session; data: UserData[] | [] }> = ({
           {/* Content */}
           <section className="sm:mx-24 mx-5 flex-grow">
             {/* Descriptive Heading */}
-            <div className="mb-4 space-x-2 dark:space-x-0 dark:space-y-2 sm:space-x-6">
-              <h2 className="text-rakhadi dark:text-center dark:w-full dark:text-slate-400 inline-block font-semibold text-base sm:text-2xl">
+            <div className="mb-4 dark:space-y-2 dark:sm:space-y-0 sm:space-x-6">
+              <h2 className="text-rakhadi dark:text-center dark:w-full dark:sm:w-auto dark:text-slate-400 inline-block font-semibold text-base sm:text-2xl">
                 Your Notes &amp; Passwords
               </h2>
               <button
                 onClick={() => setNewCredsPopupState(true)}
-                className="p-2 text-slate-500 dark:mx-auto dark:w-full text-sm sm:text-base bg-neutral-500/10 hover:outline hover:outline-2 hover:text-slate-600 hover:outline-rakhadi/30 hover:shadow-neutral-200 hover:drop-shadow-sm active:text-white active:outline-none active:bg-rakhadi dark:active:bg-slate-500 dark:bg-slate-600 dark:text-slate-300/80 dark:hover:text-slate-200 dark:font-semibold transition font-medium rounded-md backdrop-blur-3xl"
+                className="p-2 text-slate-500 dark:mx-auto dark:w-full dark:sm:w-auto text-sm sm:text-base bg-neutral-500/10 hover:outline hover:outline-2 hover:text-slate-600 hover:outline-rakhadi/30 hover:shadow-neutral-200 hover:drop-shadow-sm active:text-white active:outline-none active:bg-rakhadi dark:active:bg-slate-500 dark:bg-slate-600 dark:text-slate-300/80 dark:hover:text-slate-200 dark:font-semibold transition font-medium rounded-md backdrop-blur-3xl"
               >
                 Add Credentials
               </button>
